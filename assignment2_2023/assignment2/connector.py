bcb4550bc7253f9d0a961baef9781ec47228819c
Arash Aminpour <arasham@stud.ntnu.no>
2023-09-20 16:34:34 +0200

    updates

--- assignment2_2023/assignment2/connector.py
+++
@@ -1,8 +0,0 @@
-from DbConnector import DbConnector
-
-
-class connector: 
-    def __init__(self): 
-        self.connection = DbConnector()
-        self.db_connection = self.connection.db_connection
-        self.cursor = self.connection.cursor 
